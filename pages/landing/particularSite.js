4338e1db7299efe2d687e864b216c275aa7cee0e
Anuj Rajaram Jadhav <76788689+anuj1511@users.noreply.github.com>
2022-03-29 00:59:46 +0530

    Payment histroy table

--- pages/landing/particularSite.js
+++ pages/landing/particularSite.js
@@ -1,3 +1,176 @@
+// import Head from "next/head";
+// import Header from "../components/Header";
+// import RentersList from "./components/RentersList";
+// import Image from "next/image";
+// import ParticularSite from "../../public/images/ParticularSite.png";
+// import NameLabel from "../components/NameLabel";
+// import AllotPopup from "./components/AllotSite";
+// import React, { useContext, useEffect, useState } from "react";
+// import { Store } from "../../utility/Store";
+// import Cookies from "js-cookie";
+// import { useSnackbar } from "notistack";
+// import { useRouter } from "next/router";
+// import axios from "axios";
+// import * as ReactBootStrap from "react-bootstrap";
+// import NotLoggedIn from "../withoutLogin/NotLoggedIn";
+// import ChargesList from "./components/ChargesList";
+// import Taskbar from "../profile/components/Taskbar";
+
+// export default function ParticularSiteComponent() {
+//   const router = useRouter();
+//   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+//   const { dispatch, state } = useContext(Store);
+//   const [loading, setLoading] = useState(false);
+
+//   useEffect(() => {
+//     getSite();
+//     getParticularSiteCharges();
+//   }, []);
+
+//   const getSite = async () => {
+//     if (Cookies.get("userInfo")) {
+//       closeSnackbar();
+
+//       let config = {
+//         headers: {
+//           authorization: "b " + JSON.parse(Cookies.get("userInfo")).data.token,
+//         },
+//       };
+
+//       try {
+//         await axios.get(`/api/site/${router.query.id}`, config).then((res) => {
+//           dispatch({
+//             type: "GET_PARTICULAR_SITE",
+//             payload: res.data,
+//           });
+//           setLoading(true);
+//         });
+//         // enqueueSnackbar("Site Loaded", { variant: "success" });
+//       } catch (err) {
+//         setLoading(true);
+//         enqueueSnackbar(err.response?.data?.message, { variant: "error" });
+//       }
+//     } else {
+//       setLoading(true);
+//       enqueueSnackbar("Signup/signin Required", { varient: "success" });
+//     }
+//   };
+
+//   const getParticularSiteCharges = async () => {
+//     if (Cookies.get("userInfo")) {
+//       closeSnackbar();
+//       let config = {
+//         headers: {
+//           authorization: "b " + JSON.parse(Cookies.get("userInfo")).data.token,
+//         },
+//       };
+//       try {
+//         await axios
+//           .post(
+//             "/api/charges/view",
+//             {
+//               siteId: router.query?.id,
+//             },
+//             config
+//           )
+//           .then((res) => {
+//             dispatch({
+//               type: "GET_PARTICULAR_SITE_CHARGES",
+//               payload: res.data,
+//             });
+//             enqueueSnackbar("Charges Retrived", { variant: "success" });
+//           });
+//       } catch (err) {
+//         enqueueSnackbar(err.response?.data?.message, { variant: "error" });
+//       }
+//     } else {
+//       enqueueSnackbar("Login/Signup Required", { varient: "success" });
+//     }
+//   };
+
+//   console.log(state.siteDetail);
+//   return (
+//     <>
+//       <div className="Parent">
+//         <Taskbar />
+//         <div className="S_right">
+//           <Head>
+//             <title>Particular Site</title>
+//           </Head>
+//           {loading ? (
+//             <div className="p_sitepage">
+//               <Header header="Site Details" />
+//               <link
+//                 rel="stylesheet"
+//                 href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+//                 integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+//                 crossOrigin="anonymous"
+//               />
+//               <div className="p_site">
+//                 <div className="p_sitecontainer">
+//                   <div>
+//                     <NameLabel
+//                       label="Site Name"
+//                       details={state.siteDetail?.alias_name}
+//                     />
+//                   </div>
+//                   <div>
+//                     <NameLabel
+//                       label="Site Type"
+//                       details={state.siteDetail?.Type}
+//                     />
+//                   </div>
+//                   <div>
+//                     <NameLabel
+//                       label="Site Address"
+//                       details={`${state.siteDetail?.address?.first_line}, ${state.siteDetail?.address?.landmark}, ${state.siteDetail?.address?.city}, ${state.siteDetail?.address?.state} P.O: ${state.siteDetail?.address?.pincode}`}
+//                     />
+//                   </div>
+//                 </div>
+//                 <div className="p_psite">
+//                   <Image src={ParticularSite} alt="sub" />
+//                 </div>
+//               </div>
+//               <div className="row">
+//                 <div className="col-sm-12 col-lg-12 col-12">
+//                   <div className="p_particular">
+//                     {state.siteDetail &&
+//                     state.siteDetail.history &&
+//                     state.siteDetail.history.length > 0 ? (
+//                       <RentersList
+//                         head="Renters Alloted"
+//                         historyDetail={state.siteDetail?.history}
+//                         rent={state.siteDetail?.rent}
+//                         deposit={state.siteDetail?.deposit}
+//                         tenantsDetail={state.siteDetail?.tenantsDetails}
+//                         flat="Flat No."
+//                         loc="Location"
+//                         rentedFrom="RentedFrom"
+//                         rentedTill="Rented Till"
+//                         rents="Rent"
+//                       />
+//                     ) : (
+//                       "There no Tenant for this site"
+//                     )}
+//                   </div>
+//                   <AllotPopup
+//                     siteId={state.siteDetail?._id}
+//                     siteStatus={state.siteDetail?.status}
+//                   />
+//                 </div>
+//               </div>
+//             </div>
+//           ) : (
+//             <div className="p_spinner">
+//               <ReactBootStrap.Spinner animation="border" />
+//             </div>
+//           )}
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
 import Head from "next/head";
 import Header from "../components/Header";
 import RentersList from "./components/RentersList";
@@ -12,9 +185,7 @@ import { useSnackbar } from "notistack";
 import { useRouter } from "next/router";
 import axios from "axios";
 import * as ReactBootStrap from "react-bootstrap";
-import NotLoggedIn from "../withoutLogin/NotLoggedIn";
 import ChargesList from "./components/ChargesList";
-import Taskbar from "../profile/components/Taskbar";
 
 export default function ParticularSiteComponent() {
   const router = useRouter();
@@ -45,13 +216,12 @@ export default function ParticularSiteComponent() {
           });
           setLoading(true);
         });
+
         // enqueueSnackbar("Site Loaded", { variant: "success" });
       } catch (err) {
-        setLoading(true);
         enqueueSnackbar(err.response?.data?.message, { variant: "error" });
       }
     } else {
-      setLoading(true);
       enqueueSnackbar("Signup/signin Required", { varient: "success" });
     }
   };
@@ -88,85 +258,97 @@ export default function ParticularSiteComponent() {
     }
   };
 
-  console.log(state.siteDetail);
+  console.log(state.siteCharges);
+
   return (
     <>
-      <div className="Parent">
-        <Taskbar />
-        <div className="S_right">
-          <Head>
-            <title>Particular Site</title>
-          </Head>
-          {loading ? (
-            <div className="p_sitepage">
-              <Header header="Site Details" />
-              <link
-                rel="stylesheet"
-                href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-                integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
-                crossOrigin="anonymous"
-              />
-              <div className="p_site">
-                <div className="p_sitecontainer">
-                  <div>
-                    <NameLabel
-                      label="Site Name"
-                      details={state.siteDetail?.alias_name}
-                    />
-                  </div>
-                  <div>
-                    <NameLabel
-                      label="Site Type"
-                      details={state.siteDetail?.Type}
-                    />
-                  </div>
-                  <div>
-                    <NameLabel
-                      label="Site Address"
-                      details={`${state.siteDetail?.address?.first_line}, ${state.siteDetail?.address?.landmark}, ${state.siteDetail?.address?.city}, ${state.siteDetail?.address?.state} P.O: ${state.siteDetail?.address?.pincode}`}
-                    />
-                  </div>
-                </div>
-                <div className="p_psite">
-                  <Image src={ParticularSite} alt="sub" />
-                </div>
+      <Head>
+        <title>Particular Site</title>
+      </Head>
+      {loading ? (
+        <div className="p_sitepage">
+          <Header header="Site Details" />
+          <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+            integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+            crossOrigin="anonymous"
+          />
+          <div className="p_site">
+            <div className="p_sitecontainer">
+              <div>
+                <NameLabel
+                  label="Site Name"
+                  details={state.siteDetail?.alias_name}
+                />
               </div>
-              <div className="row">
-                <div className="col-sm-12 col-lg-12 col-12">
-                  <div className="p_particular">
-                    {state.siteDetail &&
-                    state.siteDetail.history &&
-                    state.siteDetail.history.length > 0 ? (
-                      <RentersList
-                        head="Renters Alloted"
-                        historyDetail={state.siteDetail?.history}
-                        rent={state.siteDetail?.rent}
-                        deposit={state.siteDetail?.deposit}
-                        tenantsDetail={state.siteDetail?.tenantsDetails}
-                        flat="Flat No."
-                        loc="Location"
-                        rentedFrom="RentedFrom"
-                        rentedTill="Rented Till"
-                        rents="Rent"
-                      />
-                    ) : (
-                      "There no Tenant for this site"
-                    )}
-                  </div>
-                  <AllotPopup
-                    siteId={state.siteDetail?._id}
-                    siteStatus={state.siteDetail?.status}
+              <div>
+                <NameLabel label="Site Type" details={state.siteDetail?.Type} />
+              </div>
+              <div>
+                <span className="p_label">Address:</span>
+                <textarea
+                  readOnly
+                  className="p_textarea"
+                  id="story"
+                  name="story"
+                  rows="5"
+                  cols="33"
+                  value={`${state.siteDetail?.address?.first_line}, ${state.siteDetail?.address?.landmark}, ${state.siteDetail?.address?.city}, ${state.siteDetail?.address?.state}, ${state.siteDetail?.address?.country} P.O: ${state.siteDetail?.address?.pincode}`}
+                ></textarea>
+              </div>
+            </div>
+            <div className="p_psite">
+              <Image src={ParticularSite} alt="sub" />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-12 col-lg-12 col-12">
+              <div className="p_particular">
+                {state.siteDetail.current_tenant?.length > 0 ? (
+                  <RentersList
+                    head="Renters Alloted"
+                    tenantDetails={state.siteDetail?.current_tenant[0]}
+                    historyDetail={state.siteDetail?.history[0]}
+                    rent={state.siteDetail?.rent}
+                    deposit={state.siteDetail?.deposit}
+                    flat="Flat No."
+                    loc="Location"
+                    rentedFrom="RentedFrom"
+                    rentedTill="Rented Till"
                   />
-                </div>
+                ) : (
+                  "There no Tenant for this site"
+                )}
               </div>
             </div>
-          ) : (
-            <div className="p_spinner">
-              <ReactBootStrap.Spinner animation="border" />
+            <div className="col-sm-12 col-lg-12 col-12 mt-5">
+              <div className="p_particular">
+                {state.siteCharges?.length > 0 ? (
+                  <ChargesList
+                    head="Payment History"
+                    chargesDetails={state.siteCharges}
+                  />
+                ) : (
+                  "No charges to display, generate charges to see charges here"
+                )}
+              </div>
             </div>
-          )}
+          </div>
+
+          {/* <div className='btn3'>
+                    <button className='btn1 p_btr'>Add New Tenant</button>
+                </div> */}
+          <AllotPopup
+            siteId={state.siteDetail?._id}
+            siteStatus={state.siteDetail?.status}
+          />
         </div>
-      </div>
+      ) : (
+        <div className="p_spinner">
+          <ReactBootStrap.Spinner animation="border" />
+        </div>
+      )}
     </>
   );
 }
