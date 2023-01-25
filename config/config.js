646681b800bde275e716056d5763df94c68a4611
Anuj Rajaram Jadhav <76788689+anuj1511@users.noreply.github.com>
2023-01-25 12:08:25 +0530

    current correct

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 let config = {
-  MONGO_URL:
-    "mongodb+srv://admin:admin@cluster0.evrtj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+
+  MONGO_URL: `mongodb+srv://Anuj:Anuj@database.34bt3.mongodb.net/?retryWrites=true&w=majority`,
   DB_CONNECTION_RETTEMPT_LIMIT_NODE: 5,
   SECRET_KEY: "jksdo",
 };
